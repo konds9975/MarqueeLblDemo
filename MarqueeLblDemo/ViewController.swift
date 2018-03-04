29b59803467144e50ad698062c3b36e3bd752df8
Kondiram Sonawane <Kondya@Kondirams-MacBook-Air.local>
2018-03-04 11:33:27 +0530

    commit

--- MarqueeLblDemo/ViewController.swift
+++ MarqueeLblDemo/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var marqueeLbl: MarqueeLabel!
+    
+    
+    @IBOutlet weak var marqueeLbl1: MarqueeLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    //First Lbl
+        
+        marqueeLbl1.type = .continuousReverse
+        marqueeLbl1.textAlignment = .left
+        marqueeLbl1.speed = .duration(8.0)
+        marqueeLbl1.fadeLength = 15.0
+        marqueeLbl1.leadingBuffer = 40.0
+        marqueeLbl1.text = "This is a long string text to uilable is moving ................bla bla bla ..............."
+        
+        
+        
+        ///Second
+        marqueeLbl.type = .continuous
+        marqueeLbl.textAlignment = .left
+        marqueeLbl.lineBreakMode = .byTruncatingHead
+        marqueeLbl.speed = .duration(8.0)
+        marqueeLbl.fadeLength = 15.0
+        marqueeLbl.leadingBuffer = 40.0
+        marqueeLbl.text = "This is a long string, that's also an attributed string, which works just as well!"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
